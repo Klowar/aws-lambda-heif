0ddfd7d82c33383f06b40b225d76e0a1deda25b3
Aider <ideeer4@gmail.com>
2020-01-17 22:02:12 +0300

    UPDATE server boot script

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import routes from './src/routes';
 import cors from 'cors';
+import path from 'path';
 
 const express = require('express');
+export const APP_ROOT = path.resolve(__dirname);
 
 var app = express();
 
 app.use(cors());
 app.use(routes);
-app.listen(3000);
+app.listen(4000);
