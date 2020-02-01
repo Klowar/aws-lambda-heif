71e7d08d7a43646968359e03ec943dbcf403f38e
Aider <ideeer4@gmail.com>
2020-02-01 23:09:04 +0300

    Prefix's for routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import walls from './api/wallpaper';
+import auth from './auth/user';
 
 const router = Router();
 
 router.use(`/v${1}/wallpapers/`, walls);
+router.use(`/v${1}/user/`, auth);
 
 export default router;
